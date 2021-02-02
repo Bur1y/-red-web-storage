08dab36bd5934b55b5f93ac46e177edd78151e4b
Serjoja <kotomkin52@mail.ru>
2021-02-02 20:43:14 +0300

    Написал простой сервер с маршрутизацией

---
+++ serverCFG.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle){
+
+    function onRequest(request, response) {
+
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(handle, pathname, response);
+
+    }
+
+    var port = 1707;
+    http.createServer(onRequest).listen(port);
+    console.log(`Server started on port ${port}`);
+}
+
+exports.start = start;
